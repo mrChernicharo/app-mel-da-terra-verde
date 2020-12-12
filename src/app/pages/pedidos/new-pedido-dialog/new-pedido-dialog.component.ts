9f348895804bc95a16f86e67bc530d26cf1f8546
Felipe Chernicharo <string7dev@gmail.com>
2020-12-12 04:15:11 -0300

    iniciando pedido form

--- src/app/pages/pedidos/new-pedido-dialog/new-pedido-dialog.component.ts
+++ src/app/pages/pedidos/new-pedido-dialog/new-pedido-dialog.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-pedido-dialog',
   templateUrl: './new-pedido-dialog.component.html',
-  styleUrls: ['./new-pedido-dialog.component.scss']
+  styleUrls: ['./new-pedido-dialog.component.scss'],
 })
 export class NewPedidoDialogComponent implements OnInit {
+  pedidoFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  get pedidos() {
+    return this.pedidoFormGroup.controls['consultas'] as FormArray;
   }
 
+  ngOnInit(): void {
+    this.pedidoFormGroup = this.formBuilder.group({
+      nomeCliente: new FormControl(),
+      dataPedido: new FormControl(),
+      previsaoEntrega: new FormControl(),
+      desconto: new FormControl(),
+      produtos: this.formBuilder.array([]),
+    });
+  }
 }
