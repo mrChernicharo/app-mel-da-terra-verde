589295b68836400716d76dcc7d1524cf621c02c6
Felipe Chernicharo <string7dev@gmail.com>
2020-12-22 23:16:57 -0300

    input novoMel dentro do select novaCompra

--- src/app/pages/home/compra-dialog/compra-dialog.component.ts
+++ src/app/pages/home/compra-dialog/compra-dialog.component.ts
@@ -1,20 +1,39 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldDefaultOptions,
+} from '@angular/material/form-field';
 import { EstoqueService } from 'src/app/services/estoque.service';
 
 @Component({
   selector: 'app-compra-dialog',
   templateUrl: './compra-dialog.component.html',
   styleUrls: ['./compra-dialog.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CompraDialogComponent implements OnInit {
   compraForm: FormGroup;
+  @ViewChild('inputOption') optInput: MatFormField;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public meles: string[],
     private dialogRef: MatDialogRef<CompraDialogComponent>,
-    private estoque: EstoqueService
+    private estoque: EstoqueService,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit(): void {
@@ -32,5 +51,16 @@ export class CompraDialogComponent implements OnInit {
 
   onCancel() {
     this.dialogRef.close();
+  }
+
+  addNewMel(event) {
+    const inputValue: string = event.target.value;
+
+    if (inputValue.length > 0) {
+      this.meles.push(inputValue);
+      // const formFieldEl = document.getElementById('mel-form-field');
+      // console.log(formFieldEl);
+      this.compraForm.get('mel').patchValue(inputValue);
+    }
   }
 }
